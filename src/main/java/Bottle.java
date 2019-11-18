694ffdd0649975d1d6b2a3d48e83ca9567341aa6
Iona Grant <ionagrant92@gmail.com>
2019-11-18 15:24:21 +0000

    got volume

--- src/main/java/Bottle.java
+++ src/main/java/Bottle.java
@@ -1,2 +1,13 @@
 public class Bottle {
+    private int volume;
+
+
+
+    public Bottle(int volume){
+        this.volume = 100;
+    }
+
+    public int getVolume(){
+        return this.volume;
+    }
 }
--- src/test/java/BottleTest.java
+++ src/test/java/BottleTest.java
@@ -1,2 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BottleTest {
+    Bottle bottle;
+
+    @Before
+    public void before(){
+        bottle = new Bottle(100);
+    }
+
+    @Test
+    public void volumeOf100(){
+        assertEquals(100, bottle.getVolume());
+    }
 }
