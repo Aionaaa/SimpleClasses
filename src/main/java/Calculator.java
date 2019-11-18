ff8ec75ddc1bbe7220b1456936b05b28d8a26d72
Iona Grant <ionagrant92@gmail.com>
2019-11-18 15:11:14 +0000

    initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+//    private int add;
+//    private int subtract;
+//    private int multiply;
+//    private double divide;
+
+    public Calculator(){
+
+    }
+
+    public int add(int int1, int int2) {
+        return int1 + int2;
+    }
+}
